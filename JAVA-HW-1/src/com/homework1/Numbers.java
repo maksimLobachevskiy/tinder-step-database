0dfe660cb400e6c8132adc4a02cd1e0b761c6edd
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2021-09-28 21:56:38 +0300

    hw1

---
+++ JAVA-HW-1/src/com/homework1/Numbers.java
@@ -0,0 +1,27 @@
+package com.numbers;
+
+import java.util.Scanner;
+
+public class Numbers {
+  public static void main(String[] args) {
+    System.out.println("Let the game begin!");
+    Scanner myName = new Scanner(System.in);
+    System.out.print("Input your name: ");
+    String name = myName.next();
+    while (true) {
+      Scanner myNumber = new Scanner(System.in);
+      System.out.print(name + " input your number: ");
+      int num = myNumber.nextInt();
+      int a = (int) (Math.random()*100) + 1;
+      System.out.printf("Your number: %d \n", num);
+      if (num < a) {
+        System.out.println("Your number is too small. Please, try again.");
+      } else if (num > a) {
+        System.out.println("Your number is too big. Please, try again.");
+      } else {
+        System.out.println("Congratulations, " + name + "!");
+        break;
+      }
+    }
+  }
+}
